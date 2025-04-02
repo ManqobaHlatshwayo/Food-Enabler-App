1efcb99d79a8a80fc28f1b6df3da4975c4842241
lab_services_student <st10483251@vcconnect.edu.za>
2025-04-02 22:58:35 +0200

    My food app, it helps choose what meal to eat at what time of the day

---
+++ app/src/main/java/vcmsa/ci/foodsector/MainActivity.kt
@@ -0,0 +1,27 @@
+//Title: Food Enabler! MainActivity
+//Author: Manqoba Hlatshwayo
+//Date 29 March 2025
+//Version: 1.0
+
+package vcmsa.ci.foodsector
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge)
+
+        //Retrieve UI elements from the layout by their IDs.
+        //EditText for user to input name of a meal.
+        // Meal suggestion values.
+
+
+
+
+
+
